07400425ee240c275931bf86a26ad067a912681c
Zubain-Nadeem <Zubainali23@gmail.com>
2025-07-27 17:23:04 +0500

    Four header compelted

--- src/Components/Headers/Header.jsx
+++ src/Components/Headers/Header.jsx
@@ -1,4 +1,10 @@
 import React from 'react'
+// Header top section icons
+import { FaFacebookF } from "react-icons/fa6";
+import { FaLinkedinIn } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+
+
 
 const Header = () => {
     return (
@@ -51,7 +57,7 @@ const Header = () => {
             {/* Header with searchbar */}
 
             {/* Header with CTA Button */}
-<div className='border border-black mt-7 w-full h-[65px] bg-blue-950'>
+            <div className='border border-black mt-7 w-full h-[65px] bg-blue-950'>
                 <header className='w-[95%] h-[65px] mx-auto text-white flex items-center justify-between'>
                     <h1 className='text-2xl font-semibold '>[Zubain]</h1>
 
@@ -64,16 +70,53 @@ const Header = () => {
                         </ul>
                     </nav>
 
-                        <div className='flex items-center gap-7'>
-                            <button className='border-none cursor-pointer hover:bg-blue-500 transition duration-200 ease-in-out px-4 py-2 rounded-2xl text-normal bg-blue-600 outline-none'>Login</button>
-                            <button className='border-none cursor-pointer hover:bg-blue-500 transition duration-200 ease-in-out px-4 py-2 rounded-2xl text-normal bg-blue-600 outline-none'>Sign Up</button>
-                        </div>
+                    <div className='flex items-center gap-7'>
+                        <button className='border-none cursor-pointer hover:bg-blue-500 transition duration-200 ease-in-out px-4 py-2 rounded-2xl text-normal bg-blue-600 outline-none'>Login</button>
+                        <button className='border-none cursor-pointer hover:bg-blue-500 transition duration-200 ease-in-out px-4 py-2 rounded-2xl text-normal bg-blue-600 outline-none'>Sign Up</button>
+                    </div>
                 </header>
 
             </div>
 
 
             {/* Header with CTA Button */}
+
+            {/* Header wiht Top Section */}
+
+            <div className='border border-black mt-7 h-[125px] bg-blue-950 text-white flex flex-col justify-between'>
+                <div className='border-b border-slate-400 w-[95%] mx-auto flex items-center justify-between h-[60px] py-4.5'>
+
+
+                    <h1 className='text-3xl font-semibold '>[Zubain]</h1>
+
+
+                    <div className=' h-[60px] flex gap-9  items-center justify-center'>
+                        <FaFacebookF className='cursor-pointer   hover:bg-blue-600 w-[25px] h-[20px] transition duration-300 ease-in-out' />
+                        <FaLinkedinIn className='cursor-pointer   hover:bg-blue-600 w-[25px] h-[20px] transition duration-300 ease-in-out' />
+                        <FaXTwitter className='cursor-pointer   hover:bg-black w-[25px] h-[20px] transition duration-300 ease-in-out' />
+                    </div>
+                </div>
+
+
+                <nav className=' w-[95%] mx-auto h-[60px] flex justify-center items-center'>
+                    <ul className='flex gap-9'>
+                        <li><a href="#" className='hover:text-blue-300 transition duration-300 ease-in-out'>Home</a></li>
+                        <li><a href="#" className='hover:text-blue-300 transition duration-300 ease-in-out'>Contact</a></li>
+                        <li><a href="#" className='hover:text-blue-300 transition duration-300 ease-in-out'>Service</a></li>
+                        <li><a href="#" className='hover:text-blue-300 transition duration-300 ease-in-out'>About</a></li>
+                        <li><a href="#" className='hover:text-blue-300 transition duration-300 ease-in-out'>Source</a></li>
+                        <li><a href="#" className='hover:text-blue-300 transition duration-300 ease-in-out'>Store</a></li>
+                        <li><a href="#" className='hover:text-blue-300 transition duration-300 ease-in-out'>Fashion</a></li>
+                        <li><a href="#" className='hover:text-blue-300 transition duration-300 ease-in-out'>Blog</a></li>
+                        <li><a href="#" className='hover:text-blue-300 transition duration-300 ease-in-out'>Team</a></li>
+                        <li><a href="#" className='hover:text-blue-300 transition duration-300 ease-in-out'>Feature</a></li>
+                        <li><a href="#" className='hover:text-blue-300 transition duration-300 ease-in-out'>Partner</a></li>
+                    </ul>
+                </nav>
+
+
+            </div>
+            {/* Header wiht Top Section */}
 
         </div>
     )
