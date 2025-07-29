d17b26a0fd3e18ac9d5f5c7b98860a9336b9457b
Zubain-Nadeem <Zubainali23@gmail.com>
2025-07-29 15:08:10 +0500

    FLow Bite liabery added

--- src/Components/Headers/Header.jsx
+++ src/Components/Headers/Header.jsx
@@ -5,9 +5,9 @@ import { FaLinkedinIn } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 
 
-
 const Header = () => {
     return (
+
         <div>
 
             {/* Simple Header Logo + Navlinks */}
